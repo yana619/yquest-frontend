9ca88a40b35a524925ef64ee3a614459539c36c6
yana619 <yanka-gala@mail.ru>
2017-11-15 16:36:09 +0300

    Fix Reconnect Count

--- modules/ws.js
+++ modules/ws.js
@@ -13,7 +13,7 @@ export default class WS {
             format: 'json',
             store: self.store,
             reconnection: true, // (Boolean) whether to reconnect automatically (false)
-            reconnectionAttempts: 5, // (Number) number of reconnection attempts before giving up (Infinity),
+            reconnectionAttempts: 3000, // (Number) number of reconnection attempts before giving up (Infinity),
             reconnectionDelay: 3000, // (Number) how long to initially wait before attempting a new (1000)
         });
 
